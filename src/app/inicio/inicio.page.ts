41c885a317741d69ccbd040124eafe47f6068c1d
Lorenzo <81311825+Acquesta@users.noreply.github.com>
2023-08-23 16:30:14 -0300

    Add files via upload

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,22 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import auth from 'src/config/firebasedb';
+import { Component, OnInit } from "@angular/core";
+import auth from "src/config/firebasedb";
+import { onAuthStateChanged } from "firebase/auth";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "src/config/firebasedb";
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.page.html',
-  styleUrls: ['./inicio.page.scss'],
+  selector: "app-inicio",
+  templateUrl: "./inicio.page.html",
+  styleUrls: ["./inicio.page.scss"],
 })
 export class InicioPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor() {}
 
   nome: any;
 
   user = auth.currentUser;
 
-  
+  ngOnInit() {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log(uid);
 
+        const querySnapshot = await getDocs(collection(db, uid));
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+
+          const db = doc.data()
+
+          this.nome = db['numero']
+
+          console.log(this.nome)
+        });
+
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+  }
 }

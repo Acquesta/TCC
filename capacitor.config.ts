5d671d2ae721633c2aa811abe8273087d33b3a9f
Lorenzo <81311825+Acquesta@users.noreply.github.com>
2023-09-18 11:44:43 -0300

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'tcc',
+  appName: 'GesTech',
   webDir: 'www',
   server: {
     androidScheme: 'https'

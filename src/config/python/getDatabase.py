5aa54733dfd1a80d86be87157df53965510fba81
Lorenzo <81311825+Acquesta@users.noreply.github.com>
2023-10-18 13:02:42 -0300

    Add files via upload

--- src/config/python/getDatabase.py
+++ src/config/python/getDatabase.py
@@ -1,18 +1,8 @@
-import pyrebase 
+import requests
 
-config = {
-  'apiKey': "AIzaSyBbY3mR_FrGR70807_x2GQ21UHyu3WqQ5I",
-  'authDomain': "gestechdb.firebaseapp.com",
-  'databaseURL': "https://gestechdb-default-rtdb.firebaseio.com",
-  'projectId': "gestechdb",
-  'storageBucket': "gestechdb.appspot.com",
-  'messagingSenderId': "299154456680",
-  'appId': "1:299154456680:web:5d1c16fb4f8cf518e57bdd",
-  'measurementId': "G-0306LTT302"
-}
+link = 'https://gestechdb-default-rtdb.firebaseio.com/'
 
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
-results = db.child("users").get()
+r = requests.get(f'{link}.json', verify=False)
 
-print(results)
+print(r.text)
+
